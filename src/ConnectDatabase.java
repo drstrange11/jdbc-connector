0b4f74f6e6c7835a445dbfa294b2ba19a3d7474b
Ajay R <ajaysrikrishna@gmail.com>
2020-01-21 07:52:36 +0530

    package updates

--- src/ConnectDatabase.java
+++ src/ConnectDatabase.java
@@ -1,19 +1,24 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class ConnectDatabase {
     public static void main(String[] args) {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.jdbc.Driver"); //Registering the JDBC Driver for MySQL
         } catch (ClassNotFoundException e) {
             System.out.println("JDBC driver not found");
             return;
         }
         try {
+            //Establishing Connection with the driver. Add username and password if required
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/emp", "root", "");
             Statement st = conn.createStatement();
-            ResultSet res = st.executeQuery("select * from employee;");
+            ResultSet res = st.executeQuery(""); //Insert Query
             while (res.next()) {
-                System.out.println(res.getInt(1) + " " + res.getString(2));
+                System.out.println(""); //Print result
             }
             conn.close();
         } catch (SQLException e) {
